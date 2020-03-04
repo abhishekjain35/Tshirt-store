25a3debbcec94305a1c65e76b7f128765ae19f85
Abhishek Jain <abhishekjai35@gmail.com>
2020-03-04 20:35:44 +0530

    Implemented Virtual schema and authenticate method.

--- projbackend/models/user.js
+++ projbackend/models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const crypto = require("crypto");
+const uuidv1 = require("uuid/v1");
 
 let userSchema = new mongoose.Schema({
   name: {
@@ -38,7 +39,22 @@ let userSchema = new mongoose.Schema({
   }
 });
 
+userSchema
+  .virtual("password")
+  .set(function(password) {
+    this._password = password;
+    this.salt = uuidv1();
+    this.encry_password = this.securePassword(password);
+  })
+  .get(function() {
+    return this._password;
+  });
+
 userSchema.method = {
+  authenticate: function(plainpassword) {
+    return this.securePassword(plainpassword) === this.encry_password;
+  },
+
   securePassword: function(plainpassword) {
     if (!password) return "";
     try {
