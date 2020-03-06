869ec7610d6c17bb281f3a19084d22254046c4d3
Abhishek Jain <abhishekjai35@gmail.com>
2020-03-06 21:48:58 +0530

    Implemented signup feature and return selective properties to client

--- projbackend/controllers/auth.js
+++ projbackend/controllers/auth.js
@@ -1,9 +1,20 @@
+const User = require("../models/user");
+
 exports.signup = (req, res) => {
-  console.log("REQ BODY", req.body)
-  res.json({
-    message: "Signup Working!"
-  })
-}
+  const user = new User(req.body);
+  user.save((err, user) => {
+    if (err) {
+      return res.status(400).send({
+        err: "Not Able to save user in DB."
+      });
+    }
+    res.json({
+      name: user.name,
+      email: user.email,
+      id: user._id
+    });
+  });
+};
 
 exports.signout = (req, res) => {
   res.json({
