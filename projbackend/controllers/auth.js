4e93cbff5faedf52b989eea8898689da22abbb07
Abhishek Jain <abhishekjai35@gmail.com>
2020-03-06 21:29:47 +0530

    Implemented signup route

--- projbackend/controllers/auth.js
+++ projbackend/controllers/auth.js
@@ -1,7 +1,11 @@
-
+exports.signup = (req, res) => {
+  res.json({
+    message: "Signup Working!"
+  })
+}
 
 exports.signout = (req, res) => {
-    res.json({
-      message: "User signout"
-    });
-  };
+  res.json({
+    message: "User signout"
+  });
+};
