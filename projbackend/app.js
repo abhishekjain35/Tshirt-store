0a2ca84f5f88bdca1c5584f910c0a077134b31f0
Abhishek Jain <abhishekjai35@gmail.com>
2020-03-06 20:45:21 +0530

    Added controller for authentication

--- projbackend/app.js
+++ projbackend/app.js
@@ -5,7 +5,9 @@ const app = express();
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
+const authRoutes = require("./routes/auth");
 
+//Connecting to database
 mongoose
   .connect(process.env.DATABASE, {
     useNewUrlParser: true,
@@ -15,12 +17,18 @@ mongoose
   .then(() => console.log("DB CONNECTED"))
   .catch(() => console.log("DB NOT CONNECTED"));
 
+//Middleware
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(cors());
 
+//Routes
+app.use("/api", authRoutes);
+
+//Port
 const port = 8000;
 
+//Starting the Server
 app.listen(port, () => {
   console.log(`app is running at ${port}`);
 });
---
+++ projbackend/routes/auth.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const router = express.Router();
+const {signout} = require("../controllers/auth")
+
+router.get("/signout", signout);
+
+module.exports = router;
