bd8fa51c4e193ea72840e3d1a1d22663cbf4f294
Abhishek Jain <abhishekjai35@gmail.com>
2020-03-07 19:58:22 +0530

    Implemented jwt and proper response to the frontend

--- projbackend/controllers/auth.js
+++ projbackend/controllers/auth.js
@@ -1,5 +1,7 @@
 const User = require("../models/user");
 const { validationResult } = require("express-validator");
+const jwt = require("jsonwebtoken");
+const expressJwt = require("express-jwt");
 
 exports.signup = (req, res) => {
   const errors = validationResult(req);
@@ -35,18 +37,23 @@ exports.signin = (req, res) => {
     });
   }
 
-  User.findOne({email}, (err, user) => {
-    if(err){
+  User.findOne({ email }, (err, user) => {
+    if (err) {
       return res.status(400).json({
         error: "User email does not exist"
-      })
+      });
     }
-    if(!user.authenticate(password)){
+    if (!user.authenticate(password)) {
       return res.status(401).json({
         error: "Email and password does not match."
-      })
+      });
     }
-  })
+
+    const token = jwt.sign({ _id: user._id }, process.env.SECRET);
+    res.cookie("token", token, { expire: new Date() + 9999 });
+    const { _id, name, email, role } = user;
+    return res.json({ token, user: { _id, name, email, role } });
+  });
 };
 
 exports.signout = (req, res) => {
