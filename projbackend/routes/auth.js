c4456a197c1035933cdcbeb71fd446cb5a6dc309
Abhishek Jain <abhishekjai35@gmail.com>
2020-03-07 13:41:58 +0530

    Added validation with express-validator

--- projbackend/routes/auth.js
+++ projbackend/routes/auth.js
@@ -1,9 +1,19 @@
 const express = require("express");
 const router = express.Router();
 const { signout, signup } = require("../controllers/auth");
+const { check } = require("express-validator");
 
-router.post("/signup", signup);
-
+router.post(
+  "/signup",
+  [
+    check("name", "name should be atleast 3 chars.").isLength({ min: 3 }),
+    check("email", "email is required").isEmail(),
+    check("password", "password should be atleast 3 chars.").isLength({
+      min: 3
+    })
+  ],
+  signup
+);
 router.get("/signout", signout);
 
 module.exports = router;
