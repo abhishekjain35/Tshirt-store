266d8f6e13b8a3bbfbc4fd8b92ea9832daf4d995
Abhishek Jain <abhishekjai35@gmail.com>
2020-03-07 19:51:16 +0530

    Added signin controller and checking if there are any errors

--- projbackend/controllers/auth.js
+++ projbackend/controllers/auth.js
@@ -24,6 +24,31 @@ exports.signup = (req, res) => {
   });
 };
 
+exports.signin = (req, res) => {
+  const { email, password } = req.body;
+
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(422).json({
+      error: errors.array()[0].msg
+    });
+  }
+
+  User.findOne({email}, (err, user) => {
+    if(err){
+      return res.status(400).json({
+        error: "User email does not exist"
+      })
+    }
+    if(!user.authenticate(password)){
+      return res.status(401).json({
+        error: "Email and password does not match."
+      })
+    }
+  })
+};
+
 exports.signout = (req, res) => {
   res.json({
     message: "User signout"
