f6b4abd6966f4104274103d13ffd2685f52aa929
Abhishek Jain <abhishekjai35@gmail.com>
2020-03-15 20:43:20 +0530

    Implemented controller for createProduct route

--- projbackend/controllers/product.js
+++ projbackend/controllers/product.js
@@ -1,19 +1,53 @@
 const Product = require("../models/product");
-const formidable = require("formidable")
-const _ = require("lodash")
-const fs = require("fs")
+const formidable = require("formidable");
+const _ = require("lodash");
+const fs = require("fs");
 
 exports.getProductById = (req, res, next, id) => {
-    Product.findById(id).populate("category").exec((err, product) => {
-        if (err) {
-            return res.status(400).json({
-                error: "Product not found"
-            });
-        }
-        req.product = product;
-        next();
-    });
+    Product.findById(id)
+        .populate("category")
+        .exec((err, product) => {
+            if (err) {
+                return res.status(400).json({
+                    error: "Product not found"
+                });
+            }
+            req.product = product;
+            next();
+        });
 };
 
 exports.createProduct = (req, res) => {
-}
+    let form = new formidable.IncomingForm();
+    form.keepExtensions = true;
+
+    form.parse(req, (err, fields, file) => {
+        if (err) {
+            return res.status(400).json({
+                error: "Problem with image"
+            });
+        }
+        let product = new Product(fields);
+
+        //handling files
+        if (file.photo) {
+            if (file.photo.size > 3000000) {
+                return res.status(400).json({
+                    error: "File Size is too big"
+                });
+            }
+            product.photo.data = fs.readFileSync(file.photo.path);
+            product.photo.contentType = file.photo.type;
+        }
+
+        // Save to DB
+        product.save((err, product) => {
+            if (err) {
+                return res.status(400).json({
+                    error: "Saving item to DB failed"
+                });
+            }
+            res.json(product);
+        });
+    });
+};
