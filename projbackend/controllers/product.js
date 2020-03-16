6b2552785d69734b6fb7187a23deee68b6dc3aad
Abhishek Jain <abhishekjai35@gmail.com>
2020-03-16 20:41:12 +0530

    Implemented controller for put route

--- projbackend/controllers/product.js
+++ projbackend/controllers/product.js
@@ -83,7 +83,44 @@ exports.deleteProduct = (req, res) => {
         res.json({
             message: "Successfully deleted product",
             deletedProduct
-        })
+        });
     });
 };
 
+exports.updateProduct = (req, res) => {
+    let form = new formidable.IncomingForm();
+    form.keepExtensions = true;
+
+    form.parse(req, (err, fields, file) => {
+        if (err) {
+            return res.status(400).json({
+                error: "Problem with image"
+            });
+        }
+        const { name, description, price, category, stock } = fields;
+
+        let product = req.product;
+        product = _.extend(product, fields);
+
+        //handling files
+        if (file.photo) {
+            if (file.photo.size > 3000000) {
+                return res.status(400).json({
+                    error: "File Size is too big"
+                });
+            }
+            product.photo.data = fs.readFileSync(file.photo.path);
+            product.photo.contentType = file.photo.type;
+        }
+
+        // Save to DB
+        product.save((err, product) => {
+            if (err) {
+                return res.status(400).json({
+                    error: "Updation of the product failed"
+                });
+            }
+            res.json(product);
+        });
+    });
+};
