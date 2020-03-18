12ae6f26a04952e25709620fc461ae8c2517aa7a
Abhishek Jain <abhishekjai35@gmail.com>
2020-03-18 20:09:53 +0530

    Added controller for getAllOrders

--- projbackend/controllers/order.js
+++ projbackend/controllers/order.js
@@ -18,11 +18,24 @@ exports.createOrder = (req, res) => {
     req.body.order.user = req.profile;
     const order = new Order(req.body.order);
     order.save((err, order) => {
-        if(err){
+        if (err) {
             return res.status(400).json({
                 error: "Failed to save order in DB"
-            })
+            });
         }
-        res.json(order)
-    })
+        res.json(order);
+    });
+};
+
+exports.getAllOrders = (req, res) => {
+    Order.find()
+        .populate("user", "_id name")
+        .exec((err, orders) => {
+            if (err) {
+                return res.status(400).json({
+                    error: "No orders found in DB"
+                });
+            }
+            res.json(order);
+        });
 };
