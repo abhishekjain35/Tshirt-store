909e6fcefeee417fc5e340bf29c51cc61a16eb79
Abhishek Jain <abhishekjai35@gmail.com>
2020-03-18 19:05:40 +0530

    Added post route for creating orders and added controller for it.

--- projbackend/routes/order.js
+++ projbackend/routes/order.js
@@ -3,12 +3,20 @@ const router = express.Router();
 const { isSignedIn, isAuthenticated, isAdmin } = require("../controllers/auth");
 const { getUserById, pushOrderInPurchaseList } = require("../controllers/user");
 const { updateStock } = require("../controllers/product");
-const { getOrderById } = require("../controllers/order");
+const { getOrderById, createOrder } = require("../controllers/order");
 
 //Params
 router.param("userId", getUserById);
 router.param("orderId", getOrderById);
 
-//
+//create
+router.post(
+    "order/create/:userId",
+    isSignedIn,
+    isAuthenticated,
+    pushOrderInPurchaseList,
+    updateStock,
+    createOrder
+);
 
 module.exports = router;
