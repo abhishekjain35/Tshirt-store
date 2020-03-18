54a5bcc13bf5ff1114ed2a307542bf626a9c9ce5
Abhishek Jain <abhishekjai35@gmail.com>
2020-03-18 15:45:05 +0530

    Added getOrderById middleware

--- projbackend/app.js
+++ projbackend/app.js
@@ -9,6 +9,7 @@ const authRoutes = require("./routes/auth");
 const userRoutes = require("./routes/user");
 const categoryRoutes = require("./routes/category");
 const productRoutes = require("./routes/product");
+const orderRoutes = require("./routes/order");
 
 //Connecting to database
 mongoose
@@ -30,6 +31,7 @@ app.use("/api", authRoutes);
 app.use("/api", userRoutes);
 app.use("/api", categoryRoutes);
 app.use("/api",productRoutes);
+app.use("/api",orderRoutes);
 
 //Port
 const port = 8000;
--- projbackend/routes/order.js
+++ projbackend/routes/order.js
@@ -3,4 +3,12 @@ const router = express.Router();
 const { isSignedIn, isAuthenticated, isAdmin } = require("../controllers/auth");
 const { getUserById, pushOrderInPurchaseList } = require("../controllers/user");
 const { updateStock } = require("../controllers/product");
-const {} = require("../controllers/order");
+const { getOrderById } = require("../controllers/order");
+
+//Params
+router.param("userId", getUserById);
+router.param("orderId", getOrderById);
+
+//
+
+module.exports = router;
