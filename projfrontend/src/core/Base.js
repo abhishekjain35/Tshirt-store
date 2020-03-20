7b23dd9eeb715ba23571ef175f5a31b9cee6d0b1
Abhishek Jain <abhishekjai35@gmail.com>
2020-03-20 21:27:19 +0530

    Implemented Signup form and fixed Base component styling

--- projfrontend/src/core/Base.js
+++ projfrontend/src/core/Base.js
@@ -4,20 +4,20 @@ import Menu from "./Menu";
 const Base = ({
     title = "My Title",
     description = "My description",
-    className = "bg-dark text-white p-4",
+    className = "bg-dark text-white px-4",
     children
 }) => {
     return (
         <div>
             <Menu />
             <div className="container-fluid">
-                <div className="jumbotron bg-dark text-white text-center">
-                    <h2 className="display-4">{title}</h2>
+                <div className="jumbotron bg-dark text-white text-center py-5 mb-0">
+                    <h3 className="display-4">{title}</h3>
                     <p className="lead">{description}</p>
                 </div>
                 <div className={className}>{children}</div>
             </div>
-            <footer className="footer bg-dark mt-auto py-3">
+            <footer className="footer bg-dark mt-auto py-5">
                 <div className="container-fluid bg-success text-white text-center py-3">
                     <h4>If you got any questions feel free to reach out</h4>
                     <button className="btn btn-warning btn-lg">
--- projfrontend/src/user/Signup.js
+++ projfrontend/src/user/Signup.js
@@ -2,9 +2,34 @@ import React, { useState } from "react";
 import Base from "./../core/Base";
 
 const Signup = () => {
+    const SignupForm = () => {
+        return (
+            <div className="row">
+                <div className="col-md-6 offset-sm-3 text-left">
+                    <form>
+                        <div className="form-group">
+                            <label className="text-light">Name</label>
+                            <input className="form-control" type="text" />
+                        </div>
+                        <div className="form-group">
+                            <label className="text-light">Email</label>
+                            <input className="form-control" type="email" />
+                        </div>
+                        <div className="form-group">
+                            <label className="text-light">Password</label>
+                            <input className="form-control" type="password" />
+                        </div>
+                        <button className="btn btn-success btn-block">
+                            Submit
+                        </button>
+                    </form>
+                </div>
+            </div>
+        );
+    };
     return (
         <Base title="Signup page" description="Feel free to signup!">
-            <h1>Sign up works</h1>
+            {SignupForm()}
         </Base>
     );
 };
