3bf8525b1c474fe8d377b1751a5d9011792c311d
Abhishek Jain <abhishekjai35@gmail.com>
2020-03-20 21:30:33 +0530

    Implemented Signin form for its route

--- projfrontend/src/user/Signin.js
+++ projfrontend/src/user/Signin.js
@@ -2,9 +2,30 @@ import React, { useState } from "react";
 import Base from "./../core/Base";
 
 const Signin = () => {
+    const SigninForm = () => {
+        return (
+            <div className="row">
+                <div className="col-md-6 offset-sm-3 text-left">
+                    <form>
+                        <div className="form-group">
+                            <label className="text-light">Email</label>
+                            <input className="form-control" type="email" />
+                        </div>
+                        <div className="form-group">
+                            <label className="text-light">Password</label>
+                            <input className="form-control" type="password" />
+                        </div>
+                        <button className="btn btn-success btn-block">
+                            Submit
+                        </button>
+                    </form>
+                </div>
+            </div>
+        );
+    };
     return (
         <Base title="Signin page" description="Feel free to Signin!">
-            <h1>Sign in works</h1>
+            {SigninForm()}
         </Base>
     );
 };
