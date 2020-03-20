36b2ca7f1eb8cb107c30452970c55ec6f35381c1
Abhishek Jain <abhishekjai35@gmail.com>
2020-03-20 16:25:09 +0530

    Implemented Reusable Base component which contains Header and footer for each page

--- projfrontend/src/core/Home.js
+++ projfrontend/src/core/Home.js
@@ -1,13 +1,18 @@
 import React from "react";
 import "../styles.css"
 import { API } from './../backend';
+import Base from './Base';
 
 const Home = () => {
     console.log("API IS", API)
     return (
-        <div>
-            <h1 className="text-white">Hello front end</h1>
-        </div>
+        <Base title="Home Page">
+            <div className="row">
+                <div className="col-4"></div>
+                <div className="col-4"></div>
+                <div className="col-4"></div>
+            </div>
+        </Base>
     );
 };
 
