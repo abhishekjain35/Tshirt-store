613a5b5617468c81456e7bda313ed434f509ac23
Abhishek Jain <abhishekjai35@gmail.com>
2020-03-21 12:12:37 +0530

    Fixed some styling

--- projfrontend/src/core/Base.js
+++ projfrontend/src/core/Base.js
@@ -4,20 +4,20 @@ import Menu from "./Menu";
 const Base = ({
     title = "My Title",
     description = "My description",
-    className = "bg-dark text-white px-4",
+    className = "bg-dark text-white",
     children
 }) => {
     return (
         <div>
             <Menu />
             <div className="container-fluid">
-                <div className="jumbotron bg-dark text-white text-center py-5 mb-0">
+                <div className="jumbotron bg-dark text-white text-center mb-0">
                     <h3 className="display-4">{title}</h3>
                     <p className="lead">{description}</p>
                 </div>
                 <div className={className}>{children}</div>
             </div>
-            <footer className="footer bg-dark mt-auto py-5">
+            <footer className="footer bg-dark mt-auto">
                 <div className="container-fluid bg-success text-white text-center py-3">
                     <h4>If you got any questions feel free to reach out</h4>
                     <button className="btn btn-warning btn-lg">
