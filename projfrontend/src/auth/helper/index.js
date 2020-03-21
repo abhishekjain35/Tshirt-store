737b093ed0c38e6e8a7a1e9963a1fd1fd33d9197
Abhishek Jain <abhishekjai35@gmail.com>
2020-03-21 13:07:16 +0530

    Added signout and authenticate methods

--- projfrontend/src/auth/helper/index.js
+++ projfrontend/src/auth/helper/index.js
@@ -29,3 +29,25 @@ export const signin = user => {
         })
         .catch(err => console.log(err));
 };
+
+export const authenticate = (data, next) => {
+    if (typeof window !== "undefined") {
+        localStorage.setItem("jwt", JSON.stringify(data));
+        next();
+    }
+};
+
+export const signout = next => {
+    if (typeof window !== "undefined") {
+        localStorage.removeItem("jwt");
+        next();
+
+        return fetch(`${API}/signout`, {
+            method: "GET"
+        })
+            .then(response => {
+                console.log("signout success");
+            })
+            .catch(err => console.log(err));
+    }
+};
