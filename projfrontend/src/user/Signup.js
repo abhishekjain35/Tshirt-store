e7a5c3b584a94a9db7bbaa4e9150c4240b8db4cd
Abhishek Jain <abhishekjai35@gmail.com>
2020-03-21 15:14:07 +0530

    Added onChange handler for submitting the form

--- projfrontend/src/user/Signup.js
+++ projfrontend/src/user/Signup.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import Base from "./../core/Base";
+import { signup } from "./../auth/helper/index";
 
 const Signup = () => {
     const [values, setValues] = useState({
@@ -16,6 +17,27 @@ const Signup = () => {
         setValues({ ...values, error: false, [input]: event.target.value });
     };
 
+    const onSubmit = event => {
+        event.preventDefault();
+        setValues({ ...values, error: false });
+        signup({ name, email, password })
+            .then(data => {
+                if (data.error) {
+                    setValues({ ...values, error: data.error, success: false });
+                } else {
+                    setValues({
+                        ...values,
+                        name: "",
+                        email: "",
+                        password: "",
+                        error: "",
+                        success: true
+                    });
+                }
+            })
+            .catch(console.log("Error in Signup"));
+    };
+
     const SignupForm = () => {
         return (
             <div className="row">
@@ -23,15 +45,27 @@ const Signup = () => {
                     <form>
                         <div className="form-group">
                             <label className="text-light">Name</label>
-                            <input onChange={handleChange("name")} className="form-control" type="text" />
+                            <input
+                                onChange={handleChange("name")}
+                                className="form-control"
+                                type="text"
+                            />
                         </div>
                         <div className="form-group">
                             <label className="text-light">Email</label>
-                            <input onChange={handleChange("email")} className="form-control" type="email" />
+                            <input
+                                onChange={handleChange("email")}
+                                className="form-control"
+                                type="email"
+                            />
                         </div>
                         <div className="form-group">
                             <label className="text-light">Password</label>
-                            <input onChange={handleChange("password")} className="form-control" type="password" />
+                            <input
+                                onChange={handleChange("password")}
+                                className="form-control"
+                                type="password"
+                            />
                         </div>
                         <button className="btn btn-success btn-block">
                             Submit
