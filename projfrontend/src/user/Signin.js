6a1a90cb63f27ba4a26e9cf8e8bc60ee8ff8f9a6
Abhishek Jain <abhishekjai35@gmail.com>
2020-03-22 13:48:12 +0530

    Implemented conditional rendering on the signout route

--- projfrontend/src/user/Signin.js
+++ projfrontend/src/user/Signin.js
@@ -5,8 +5,8 @@ import { signin, authenticate, isAuthenticated } from "./../auth/helper/index";
 
 const Signin = () => {
     const [values, setValues] = useState({
-        email: "",
-        password: "",
+        email: "test@gmail.com",
+        password: "12345",
         error: "",
         loading: false,
         didRedirect: false
