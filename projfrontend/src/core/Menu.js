cc228d2f06cd3c82a18f5756ba9918a72c0043e6
Abhishek Jain <abhishekjai35@gmail.com>
2020-03-22 20:38:43 +0530

    Added conditional rendering to signup and signin pages

--- projfrontend/src/core/Menu.js
+++ projfrontend/src/core/Menu.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { Link, withRouter } from "react-router-dom";
 import { signout, isAuthenticated } from "../auth/helper";
 
@@ -49,24 +49,28 @@ const Menu = ({ history }) => (
                     A. Dashboard
                 </Link>
             </li>
-            <li className="nav-item">
-                <Link
-                    style={currentTab(history, "/signup")}
-                    className="nav-link"
-                    to="/signup"
-                >
-                    Signup
-                </Link>
-            </li>
-            <li className="nav-item">
-                <Link
-                    style={currentTab(history, "/signin")}
-                    className="nav-link"
-                    to="/signin"
-                >
-                    Sign in
-                </Link>
-            </li>
+            {!isAuthenticated() && (
+                <Fragment>
+                    <li className="nav-item">
+                        <Link
+                            style={currentTab(history, "/signup")}
+                            className="nav-link"
+                            to="/signup"
+                        >
+                            Signup
+                        </Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link
+                            style={currentTab(history, "/signin")}
+                            className="nav-link"
+                            to="/signin"
+                        >
+                            Sign in
+                        </Link>
+                    </li>
+                </Fragment>
+            )}
             {isAuthenticated() && (
                 <li className="nav-item">
                     <span
