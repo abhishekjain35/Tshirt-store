b460ceb1abcaf64a40b6e51d6740e9e3b61ff359
Abhishek Jain <abhishekjai35@gmail.com>
2020-03-23 20:23:07 +0530

    Added Private routes and user dashboard in route component

--- projfrontend/src/Routes.js
+++ projfrontend/src/Routes.js
@@ -3,6 +3,10 @@ import { Route, Switch, BrowserRouter } from "react-router-dom";
 import Home from "./core/Home";
 import Signup from "./user/Signup";
 import Signin from "./user/Signin";
+import AdminRoute from "./auth/helper/AdminRoutes";
+import PrivateRoute from "./auth/helper/PrivateRoutes";
+import UserDashBoard from "./user/UserDashBoard";
+import AdminDashBoard from "./user/AdminDashBoard";
 
 const Routes = () => {
     return (
@@ -11,6 +15,16 @@ const Routes = () => {
                 <Route path="/" exact component={Home}></Route>
                 <Route path="/signup" exact component={Signup}></Route>
                 <Route path="/signin" exact component={Signin}></Route>
+                <PrivateRoute
+                    path="/user/dashboard"
+                    exact
+                    component={UserDashBoard}
+                />
+                <PrivateRoute
+                    path="/admin/dashboard"
+                    exact
+                    component={AdminDashBoard}
+                />
             </Switch>
         </BrowserRouter>
     );
