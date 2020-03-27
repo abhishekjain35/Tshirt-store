b7e16f3960fd4fc90c0be06528f637653d8104e7
Abhishek Jain <abhishekjai35@gmail.com>
2020-03-27 15:40:14 +0530

    Added Base component to addCategory route

--- projfrontend/src/admin/AddCategory.js
+++ projfrontend/src/admin/AddCategory.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React from "react";
+import Base from "../core/Base";
 
 const AddCategory = () => {
-    return ( <div>
-        <h1>Add Category here</h1>
-    </div> );
-}
- 
+    return (
+        <Base
+            title="Create a category here"
+            description="Add categories for tshirts"
+            className="container bg-info p-4"
+        >
+            <div className="row bg-white rounded">
+                <div className="col-md-8 offset-md-2">
+                    <h1>Hello</h1>
+                </div>
+            </div>
+        </Base>
+    );
+};
+
 export default AddCategory;
