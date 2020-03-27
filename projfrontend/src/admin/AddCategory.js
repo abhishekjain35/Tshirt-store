544763a5f0d5cebd56cb6767aefae2abb2d934c5
Abhishek Jain <abhishekjai35@gmail.com>
2020-03-27 20:18:11 +0530

    Added form for creating category

--- projfrontend/src/admin/AddCategory.js
+++ projfrontend/src/admin/AddCategory.js
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import Base from "../core/Base";
+import { isAuthenticated } from "../auth/helper";
+import { Link } from "react-router-dom";
 
 const AddCategory = () => {
+    const [name, setName] = useState("initialState");
+    const [error, setError] = useState(false);
+    const [success, setSuccess] = useState(false);
+
+    const { user, token } = isAuthenticated();
+
+    const categoryForm = () => (
+        <form>
+            <div className="form-group">
+                <p className="lead">Enter the Category</p>
+                <input
+                    type="text"
+                    autoFocus
+                    required
+                    className="form-control my-3"
+                    placeholder="For ex. Summer"
+                />
+                <button className="btn btn-outline-info">
+                    Create Category
+                </button>
+            </div>
+        </form>
+    );
+
     return (
         <Base
             title="Create a category here"
@@ -9,9 +35,7 @@ const AddCategory = () => {
             className="container bg-info p-4"
         >
             <div className="row bg-white rounded">
-                <div className="col-md-8 offset-md-2">
-                    <h1>Hello</h1>
-                </div>
+                <div className="col-md-8 offset-md-2">{categoryForm()}</div>
             </div>
         </Base>
     );
