57937ccdab4477f2a6c8716dbfbcf853c98df77e
Abhishek Jain <abhishekjai35@gmail.com>
2020-03-28 20:17:07 +0530

    Added api calls for get a single product and get all products

--- projfrontend/src/admin/helper/adminapicall.js
+++ projfrontend/src/admin/helper/adminapicall.js
@@ -34,3 +34,19 @@ export const createProduct = (userId, token, product) => {
         .then(res => res.json())
         .catch(err => console.log(err));
 };
+
+export const getProducts = () => {
+    return fetch(`${API}/products`, {
+        method: "GET"
+    })
+        .then(res => res.json())
+        .catch(err => console.log(err));
+};
+
+export const getAProduct = productId => {
+    return fetch(`${API}/product/${productId}`, {
+        method: "GET"
+    })
+        .then(res => res.json())
+        .catch(err => console.log(err));
+};
