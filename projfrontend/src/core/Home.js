e64341a5d772187e91016e75ae5d1b928b257fbf
Abhishek Jain <abhishekjai35@gmail.com>
2020-04-09 20:14:04 +0530

    Added reusable card component for home page products

--- projfrontend/src/core/Home.js
+++ projfrontend/src/core/Home.js
@@ -1,12 +1,15 @@
 import React from "react";
 import "../styles.css";
 import Base from "./Base";
+import Card from './Card';
 
 const Home = () => {
     return (
         <Base title="Home Page" description="Welcome to the Tshirt store">
             <div className="row">
-                <div className="col-4"></div>
+                <div className="col-4">
+                    <Card />
+                </div>
                 <div className="col-4"></div>
                 <div className="col-4"></div>
             </div>
