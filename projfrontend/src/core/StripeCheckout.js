a0c08a1325b057f37196ec53381aad929bee34a3
Abhishek Jain <abhishekjai35@gmail.com>
2020-04-13 20:03:53 +0530

    Added method to calculate total amount of the cart

--- projfrontend/src/core/StripeCheckout.js
+++ projfrontend/src/core/StripeCheckout.js
@@ -1,12 +1,34 @@
-import React, {useEffect, useState} from "react";
-import { isAuthenticated } from './../auth/helper/index';
+import React, { useEffect, useState } from "react";
+import { isAuthenticated } from "./../auth/helper/index";
 import { emptyCart, loadCart } from "./helper/cartHelper";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
-const StripeCheckout = ({products, setReload = f => f, reload=undefined}) => {
+const StripeCheckout = ({
+    products,
+    setReload = (f) => f,
+    reload = undefined,
+}) => {
+    const [data, setData] = useState({
+        loading: false,
+        success: false,
+        error: "",
+        address: "",
+    });
+
+    const token = isAuthenticated() && isAuthenticated().token;
+    const userId = isAuthenticated() && isAuthenticated().user._id;
+
+    const getFinalPrice = () => {
+        let amount = 0;
+        products.map((p) => {
+            amount = amount + p.price;
+        });
+        return amount;
+    };
+
     return (
         <div>
-            <h3 className="text-white">Stripe Checkout</h3>
+            <h3 className="text-white">{getFinalPrice()}</h3>
         </div>
     );
 };
