cd0ad258b4bb8c10eadb2a8b4d2825ed85f118e5
Abhishek Jain <abhishekjai35@gmail.com>
2020-04-17 16:11:50 +0530

    Added payment helper for all payment API calls

---
+++ projfrontend/src/core/helper/paymenthelper.js
@@ -0,0 +1,28 @@
+import { API } from "./../../backend";
+
+export const getAToken = (userId, token) => {
+    return fetch(`${API}/payment/gettoken/${userId}`, {
+        method: "GET",
+        headers: {
+            Accept: "Application/json",
+            "Content-Type": "Application/json",
+            Authorization: `Bearer ${token}`,
+        },
+    })
+        .then((res) => res.json())
+        .catch((err) => console.log(err));
+};
+
+export const processPayment = (userId, token, paymentInfo) => {
+    return fetch(`${API}/payment/braintree/${userId}`, {
+        method: "GET",
+        headers: {
+            Accept: "Application/json",
+            "Content-Type": "Application/json",
+            Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify(paymentInfo),
+    })
+        .then((res) => res.json())
+        .catch((err) => console.log(err));
+};
