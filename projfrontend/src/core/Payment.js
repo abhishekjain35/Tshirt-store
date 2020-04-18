3e5674532db75045c68e8ab6313be4a9896a0b08
Abhishek Jain <abhishekjai35@gmail.com>
2020-04-18 16:11:33 +0530

    Fully implemented braintree payments

--- projfrontend/src/core/Payment.js
+++ projfrontend/src/core/Payment.js
@@ -12,47 +12,90 @@ const Payment = ({ products, setReload = (f) => f, reload = undefined }) => {
         success: false,
         clientToken: null,
         error: "",
-        instance: {}
+        instance: {},
     });
 
-    const userId = isAuthenticated() && isAuthenticated().user._id
-    const token = isAuthenticated() && isAuthenticated().token
+    const userId = isAuthenticated() && isAuthenticated().user._id;
+    const token = isAuthenticated() && isAuthenticated().token;
 
     const getToken = (userId, token) => {
-        getAToken(userId, token).then(res => {
-            if(res.error){
-                setInfo({...info, error: res.error})
+        getAToken(userId, token).then((res) => {
+            if (res.error) {
+                setInfo({ ...info, error: res.error });
+            } else {
+                const clientToken = res.clientToken;
+                setInfo({ clientToken });
             }
-            else{
-                const clientToken = res.clientToken
-                setInfo({clientToken})
-            }
-        })
-    }
+        });
+    };
+
+    const onPurchase = () => {
+        setInfo({ loading: true });
+        let nonce;
+        let getNonce = info.instance
+            .requestPaymentMethod()
+            .then((data) => {
+                nonce = data.nonce;
+                const paymentData = {
+                    paymentMethodNonce: nonce,
+                    amount: getAmount(),
+                };
+                processPayment(userId, token, paymentData)
+                    .then((res) => {
+                        setInfo({
+                            ...info,
+                            success: res.success,
+                            loading: false,
+                         });
+                    })
+                    .catch((err) => {
+                        setInfo({ loading: false, success: false });
+                    });
+            })
+    };
 
     const showBtDropIn = () => {
         return (
             <div>
                 {info.clientToken !== null && products.length > 0 ? (
                     <div>
-                    <DropIn
-                      options={{ authorization: info.clientToken }}
-                      onInstance={(instance) => (info.instance = instance)}
-                    />
-                    <button onClick={() => {}}>Buy</button>
-                  </div>
-                ) : (<h3>Please Login or Add something to cart</h3>)}
+                        <DropIn
+                            options={{ authorization: info.clientToken }}
+                            onInstance={(instance) =>
+                                (info.instance = instance)
+                            }
+                        />
+                        <button
+                            className="btn btn-block btn-success"
+                            onClick={onPurchase}
+                        >
+                            Buy
+                        </button>
+                    </div>
+                ) : (
+                    <h3>Please Login or Add something to cart</h3>
+                )}
             </div>
-        )
-    }
+        );
+    };
 
     useEffect(() => {
-        getToken(userId, token)
+        getToken(userId, token);
     }, []);
+
+   
+
+    const getAmount = () => {
+        let amount = 0;
+        products.map((p) => {
+            amount = amount + p.price;
+        });
+        return amount;
+    };
 
     return (
         <div>
-            <h3>Test bt</h3>
+            <h3>Your Bill is {getAmount()} $</h3>
             {showBtDropIn()}
         </div>
     );
